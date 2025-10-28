7ed3c9bed71c416ce96652d7aa10918792da24f5
Yurii Malyshko <yur.malishko@gmail.com>
2025-10-28 20:26:11 +0200

    fix controllers path

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -51,10 +51,15 @@ function trans($key): string
 }
 
 spl_autoload_register(function ($class) {
-    $file = ROOT_PATH . 'app' . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+    $file = ROOT_PATH  . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+    var_dump($file);
     if (file_exists($file)) {
         include_once $file;
         return true;
     }
     return false;
 });
+
+
+$cities = new \app\controllers\CityController();
+$cities->index();
