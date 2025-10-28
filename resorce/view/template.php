5a5a7a9962f520bd95f42059979440d54d65c3ee
Ruslan <ruslandvor1998@gmail.com>
2025-10-28 19:48:30 +0200

    view template

--- resorce/view/template.php
+++ resorce/view/template.php
@@ -1,1 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang=""
+<head>
+    <meta charset="UTF-8">
+    <title><?= trans('title') ?></title>
+    <meta name="description" content="<?= trans('meta_description') ?>">
+</head>
+<body>
+<h1><?= trans('headline') ?></h1>
+<p><?= trans('body') ?></p>
+<h2><?= trans('list_headline') ?></h2>
+<ul>
+    <?php foreach ($cities as $city): ?>
+        <li><?= htmlspecialchars($city['name']) ?></li>
+    <?php endforeach; ?>
+</ul>
+<footer>
+    <p><?= trans('copyright') ?></p>
+</footer>
+</body>
+</html>
